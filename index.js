fb17ce05385b7c2f412998b6f33f238fb6a0df98
erikphinguyen <erikphinguyen@gmail.com>
2022-12-05 13:12:34 -0800

    added /test2

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.use(express.json()) // formats all responses in json
 // app.get('/', (req, res) => res.send('Hello World')); // this line allows us to test page
 app.get('/test', (req, res) => res.send('Hello World')); // can use this route to see if server works at all as well
 
+// const multiplyByTwo = () => {
+//     return 2 * 2
+// }
+
+// app.get('/test2', (req, res) => {
+//     res.send(`${multiplyByTwo()}`)
+// });
+
+
 const port = process.env.PORT || 5001; // "opens the door to kitchen"; NOT a connection, just a way in
 const callback = () => console.log(`Server is running on port ${port}`);
 app.listen(port, callback); // 1st argument is a variable, 2nd is a fx
